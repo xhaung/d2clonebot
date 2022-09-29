9b7011075ed82d42cb8d5a420827c67ccb1861fd
xhaung <32036737+xhaung@users.noreply.github.com>
2022-09-29 01:35:36 +0100

    Update d2clone_discord.py

--- d2clone_discord.py
+++ d2clone_discord.py
@@ -176,19 +176,20 @@ class D2Clone(discord.Client):
         text += "> Data courtesy of diablo2.io"
         return text
 
-client = D2Clone(intents=discord.Intents.all())        
-    
+
+"""
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game('!uberdiablo <eu/am/asi/non/ladder/hard/soft>'))
     print('We have logged in as {0.user}'.format(client))
+"""
 
 if __name__ == "__main__":
     token = os.environ.get("DISCORD_TOKEN")
 
     if token:
         print("entering process")
-       
+        client = D2Clone(intents=discord.Intents.all())               
         client.run(token)
         print("process ended")
     else:
