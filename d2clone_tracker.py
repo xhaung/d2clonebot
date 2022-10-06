8e845e3b2af235b7451e362b44f762052f5fa709
xhaung <32036737+xhaung@users.noreply.github.com>
2022-10-06 22:35:33 +0100

    Create d2clone_tracker.py

---
+++ d2clone_tracker.py
@@ -0,0 +1,51 @@
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_BASE_URL = os.environ.get("API_BASE_URL", "https://diablo2.io/dclone_api.php")
+DISCORD_CHANNEL_ID = int(os.environ.get("DISCORD_CHANNEL_ID", 0))
+TOKEN = os.environ.get("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} succesfully logged in!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if message.content == 'hello':
+        await message.channel.send(f'Hi {message.author}')
+    if message.content == 'bye':
+        await message.channel.send(f'Goodbye {message.author}')
+
+    await bot.process_commands(message)
+
+
+# Start each command with the @bot.command decorater
+@bot.command()
+async def square(ctx, arg): # The name of the function is the name of the command
+    print(arg) # this is the text that follows the command
+    await ctx.send(int(arg) ** 2) # ctx.send sends text in chat
+
+@bot.command()
+async def scrabblepoints(ctx, arg):
+    # Key for point values of each letter
+    score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+         "x": 8, "z": 10}
+    points = 0
+    # Sum the points for each letter
+    for c in arg:
+        points += score[c]
+    await ctx.send(points)
+
+
+bot.run(TOKEN)
